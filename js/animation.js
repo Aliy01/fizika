5e63ec2a57ea8e2cc392b7f8b696bac09a48d79e
MuhammadAliy <aliyibnzayir@gmail.com>
2022-11-03 13:18:23 +0500

    F

---
+++ js/animation.js
@@ -0,0 +1,6 @@
+const btnMain = document.querySelector(".btn-outline-danger"),
+    mainLogo = document.querySelector(".main-logo");
+btnMain.addEventListener("click", () => {
+    mainLogo.style.display = "none"
+
+})
