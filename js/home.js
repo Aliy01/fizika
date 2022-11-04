18b8c2bd2a2b94cde67a4017e73ecbfb7639875e
MuhammadAliy <aliyibnzayir@gmail.com>
2022-11-04 15:52:37 +0500

    endHomePageStyle

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-const homeLogo = document.querySelector(".home_logo");
+const homeLogo = document.querySelector(".home_logo"),
+    slideLeft = document.querySelector("fa-chevron-left"),
+    slideRight = document.querySelector("fa-chevron-right"),
+    slideRow = document.getElementsByTagName(".row"),
+    rowLength = getComputedStyle(slideRow);
+
+console.log(rowLength);
