34f3507e9f1599b26a12e9c9cb6828c885eefb7a
MuhammadAliy <aliyibnzayir@gmail.com>
2022-11-06 00:10:50 +0500

    aboutPageStyleEnd

--- js/dist/home.dev.js
+++ js/dist/home.dev.js
@@ -6,16 +6,27 @@ var homeLogo = document.querySelector(".home_logo"),
     slideRight = document.querySelector("fa-chevron-right"),
     // slideRow = document.getElementsByTagName(".row"),
 // rowLength = getComputedStyle(slideRow),
-aboutBtn = document.querySelector(".about-btn"),
+aboutBtn = document.querySelector("#about-btn"),
+    submit = document.querySelector(".submit"),
     aboutPage = document.querySelector(".about"),
     navbar = document.querySelector(".navbar"),
     homeBbtn = document.querySelector(".home-btn"),
     homeField = document.querySelector(".home-field");
 homeBbtn.addEventListener("click", function () {
   homeField.style.display = "flex";
-  aboutPage.style.display = "none"; // navbar.style.flexDirection = "row"
+  aboutPage.style.display = "none";
+  navbar.style.background = " #fff"; // navbar.style.flexDirection = "row"
 });
 aboutBtn.addEventListener("click", function () {
+  aboutPage.style.display = "flex";
+  homeField.style.display = "none";
+  navbar.style.display = "flex";
+  navbar.style.background = " rgba(100, 255, 255, .6)";
+  aboutPage.style.flexDirection = "column"; // navbar.style.flexDirection = "column"
+  // navbar.style.width = 20 + "px"
+  // navbar.style.height = 500 + "px"
+});
+submit.addEventListener("click", function () {
   aboutPage.style.display = "flex";
   homeField.style.display = "none";
   navbar.style.display = "flex";
--- js/home.js
+++ js/home.js
@@ -4,7 +4,8 @@ const homeLogo = document.querySelector(".home_logo"),
     slideRight = document.querySelector("fa-chevron-right"),
     // slideRow = document.getElementsByTagName(".row"),
     // rowLength = getComputedStyle(slideRow),
-    aboutBtn = document.querySelector(".about-btn"),
+    aboutBtn = document.querySelector("#about-btn"),
+    submit = document.querySelector(".submit"),
     aboutPage = document.querySelector(".about"),
     navbar = document.querySelector(".navbar"),
     homeBbtn = document.querySelector(".home-btn"),
@@ -13,11 +14,21 @@ const homeLogo = document.querySelector(".home_logo"),
 homeBbtn.addEventListener("click", () => {
     homeField.style.display = "flex"
     aboutPage.style.display = "none"
+    navbar.style.background = " #fff";
 
     // navbar.style.flexDirection = "row"
 })
 aboutBtn.addEventListener("click", () => {
-
+    aboutPage.style.display = "flex"
+    homeField.style.display = "none"
+    navbar.style.display = "flex"
+    navbar.style.background = " rgba(100, 255, 255, .6)";
+    aboutPage.style.flexDirection = "column"
+        // navbar.style.flexDirection = "column"
+        // navbar.style.width = 20 + "px"
+        // navbar.style.height = 500 + "px"
+})
+submit.addEventListener("click", () => {
     aboutPage.style.display = "flex"
     homeField.style.display = "none"
     navbar.style.display = "flex"
